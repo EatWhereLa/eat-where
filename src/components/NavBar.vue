056953b62ac87e8f0a69f0d6dbf4eeb7ca766a10
Syoongy <mastersyong@gmail.com>
2023-11-06 17:20:27 +0800

    feat: add timer header for votes and rework show/hide logic

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,16 +2,20 @@
 import { useAuthStore } from "@/stores/auth";
 import { useAuth } from "@/composables/auth";
 import { storeToRefs } from "pinia";
+import { useTimerStore } from "@/stores/timer";
+import { useRoute } from "vue-router";
 
 const { isAuthenticated } = storeToRefs(useAuthStore());
 const { logout } = useAuth();
 const logoUrl = new URL("/src/assets/logo.jpg", import.meta.url).href;
 const { showSidebar } = defineProps<{ showSidebar: boolean }>();
+const timerStore = useTimerStore();
+const route = useRoute();
+const { displayTime } = storeToRefs(timerStore);
 defineEmits(["toggleSidebar"]);
 
-function showMenuItem(itemName: string | undefined) {
-  const menuItems = ["profile", "bookmarks", "home"];
-  if (itemName && menuItems.includes(itemName)) {
+function showMenuItem(itemName: string[]) {
+  if (route.name && !itemName.includes(route.name.toString())) {
     return true;
   }
   return false;
@@ -34,34 +38,44 @@ function showMenuItem(itemName: string | undefined) {
           EatWhere!?
         </h1>
       </va-navbar-item>
-      <va-navbar-item
-        class="!hidden md:!block"
-        v-if="showMenuItem($route.name?.toString())"
-      >
+      <va-navbar-item v-if="$route.name === 'vote'">
+        <h1 class="text-2xl text-dark text-center font-medium">
+          <p class="text-primary">
+            {{ displayTime }}
+          </p>
+          <!-- <p
+            v-if="currentLocation.address === ''"
+            class="text-xs text-gray-500"
+          >
+            <va-icon name="near_me" size="1rem" class="mr-2 text-primary" />
+            {{ currPos.address }}
+          </p>
+          <p
+            v-else-if="currentLocation.address !== ''"
+            class="text-xs text-gray-500"
+          >
+            <va-icon name="near_me" size="1rem" class="mr-2 text-primary" />{{
+              currentLocation.address
+            }}
+          </p> -->
+        </h1>
+      </va-navbar-item>
+      <va-navbar-item class="!hidden md:!block" v-if="showMenuItem(['vote'])">
         <router-link to="/profile" active-class="text-primary">
           Profile
         </router-link>
       </va-navbar-item>
-      <va-navbar-item
-        class="!hidden md:!block"
-        v-if="showMenuItem($route.name?.toString())"
-      >
+      <va-navbar-item class="!hidden md:!block" v-if="showMenuItem(['vote'])">
         <router-link to="/bookmarks" active-class="text-primary">
           Bookmarks
         </router-link>
       </va-navbar-item>
-      <va-navbar-item
-        class="!hidden md:!block"
-        v-if="showMenuItem($route.name?.toString())"
-      >
+      <va-navbar-item class="!hidden md:!block" v-if="showMenuItem(['vote'])">
         <router-link to="/group" active-class="text-primary">
           Start Vote
         </router-link>
       </va-navbar-item>
-      <va-navbar-item
-        class="!hidden md:!block"
-        v-if="showMenuItem($route.name?.toString())"
-      >
+      <va-navbar-item class="!hidden md:!block" v-if="showMenuItem(['vote'])">
         <router-link to="/results" active-class="text-primary">
           Pick For Me
         </router-link>
